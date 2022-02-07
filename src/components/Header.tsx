7d86e5350ea73ac3067b40af6223d643f60ca62c
igorraknorwid <igorrakfed@gmail.com>
2022-02-07 08:33:55 +0100

    header changed

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,7 @@ function Header() {
     <header className='header'>
       <div className='header-container'>
         <h1 className='header-title'>
-          <Link to='/'> Bibliografia Ziemi Lubuskiej 1945-1985</Link>
+          <Link to='/'> Bibliografia Ziemi Lubuskiej 1945-1989</Link>
         </h1>
         <div style={{ margin: "0 50px" }}>
           <a href='https://opac.wimbp.zgora.pl/integro/catalog' target='_blank'>
